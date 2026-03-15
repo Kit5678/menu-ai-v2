0096bddff36d352a8721f777ca7e561e2311e4c1
Kit5678 <kitsakornkiki46@gmail.com>
2026-03-16 01:50:28 +0700

    Allow new Vercel domain in CORS

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://menu-3jagyvn97-kit5678s-projects.vercel.app",
+        "https://menu-nhiwxpcp8-kit5678s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
